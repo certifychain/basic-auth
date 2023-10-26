39200cd2d16c924b4dd66ee7876fdd3c0e4323c5
sam <aquaticcalf@proton.me>
2023-10-26 17:36:22 +0530

    meow

--- auth/auth/urls.py
+++ auth/auth/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     re_path('login',views.login),
     re_path('signup',views.signup),
+    re_path('test_token',views.test_token),
 ]
--- auth/auth/views.py
+++ auth/auth/views.py
@@ -1,10 +1,22 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .serializer import userSerilizer
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
 @api_view(['POST'])
 def login(request):
     return Response({})
 
 @api_view(['POST'])
 def signup(request):
+    serializer = userSeializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response({})
+
+@api_view(['GET'])
+def test_token(request):
     return Response({})
